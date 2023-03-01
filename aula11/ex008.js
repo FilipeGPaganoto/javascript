6f1280a4f17b2cc6279dd7b1d367fd7bae1044d0
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-03-01 18:45:17 -0300

    Oitavo exercício

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var velocidade = 78.2
+
+console.log(`A velocidade do seu carro é ${velocidade}KM/h`)
+
+if (velocidade > 60) {
+    console.log(`Você ultrapassou o limite!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança!`)
