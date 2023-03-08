94adc1bb35ec28665eed2788412a65b6880846bb
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-03-07 21:47:02 -0300

    Adicionei identificar genero

--- aula015/script.js
+++ aula015/script.js
@@ -8,5 +8,21 @@ function verificar() {
 
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert("[ERROR] Digite os campos novamente!")
-    } 
+    } else {
+        var fsex = document.getElementsByName("sexo")
+        var idade = ano - Number(fano.value)
+        var genero = ""
+        var img = document.createElement("img")
+        img.setAttribute("id", "foto")
+
+        if (fsex[0].checked) {
+            genero = "Homem"
+            if (idade >= )
+        } else if (fsex[1].checked) {
+            genero = "Mulher"
+        }
+        res.style.textAlign = "center"
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        
+    }
 }
